7ce73bcfb1233a99d6c23cc7edc87fdc89607e60
Pierre Chifflier <chifflier@wzdftpd.net>
2017-03-10 09:12:02 +0100

    Parse attributes (as TLVs)

--- src/radius.rs
+++ src/radius.rs
@@ -20,9 +20,31 @@ pub struct RadiusData<'a> {
     pub identifier: u8,
     pub length: u16,
     pub authenticator: &'a [u8], // 16 bytes
-    pub attributes: Option<&'a[u8]>,
+    pub attributes: Option<Vec<RadiusAttribute<'a>>>,
 }
 
+#[derive(Debug,PartialEq)]
+pub struct RadiusAttribute<'a> {
+    pub typ: u8,
+    pub len: u8,
+    pub val: &'a [u8],
+}
+
+
+pub fn parse_radius_attribute(i:&[u8]) -> IResult<&[u8],RadiusAttribute> {
+    do_parse!(i,
+        t: be_u8 >>
+        l: be_u8 >>
+        v: take!(l-2) >>
+        (
+            RadiusAttribute {
+                typ: t,
+                len: l,
+                val: v,
+            }
+        )
+    )
+}
 
 pub fn parse_radius_data(i:&[u8]) -> IResult<&[u8],RadiusData> {
     do_parse!(i,
@@ -30,7 +52,9 @@ pub fn parse_radius_data(i:&[u8]) -> IResult<&[u8],RadiusData> {
         id:   be_u8 >>
         len:  be_u16 >>
         auth: take!(16) >>
-        attr: cond!(len > 20, take!(len - 20)) >>
+        attr: cond!(len > 20,
+                    flat_map!(take!(len - 20),complete!(many1!(parse_radius_attribute)))
+        ) >>
         (
             RadiusData {
                 code: c,
